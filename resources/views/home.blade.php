1e579c3eddb30913eba3f23a991e67de4be045db
Auti <austin@austinolijar.com>
2024-12-01 14:45:41 -0600

    fixed header padding on links

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,13 +1,13 @@
 <x-layout>
-    <main class="bg-black h-fit w-screen pl-16 pb-12 relative">
-        <section class="flex items-start overflow-hidden h-[calc(100vh_-_64px)]">
+    <main id="main" class="main h-fit w-screen pl-16 pb-12 relative">
+        <section id="section--home-hero" class="section--home-hero flex items-start overflow-hidden h-[calc(100vh_-_64px)]">
             <div class="h-full z-10 flex flex-col justify-evenly w-5/6 pl-32">
                 <div >
                     <h1 class="whitespace-nowrap text-[12rem] leading-none font-extralight">Can I 
                     <br>borrow 
                     <br>a 
                     <div id="hero-word-rotation--container" class="inline-block w-fit absolute ml-5 h-[10px] overflow-hidden pb-[13rem]">
-                        <span class="text-red-500 block">pencil<span class="text-red-500">?</span></span>
+                        <span class="text-red-600 block">pencil<span class="text-red-600">?</span></span>
                         <span class="text-blue-500 block" style="opacity: 0">notebook<span class="text-blue-500">?</span></span>
                     </div>
                     </h1>
@@ -17,17 +17,17 @@
                     <p class="w-4/6 text-left tracking-wide font-thin text-[2rem]">An easy and effortless community resource for those who are struggling to find the supplies needed to succeed in school and educational endeavors</p>
                 </div>
                 <div class="flex w-full items-center">
-                    <a class="mr-6 text-center text-[2rem] w-full px-10 py-4 lg:py-8 rounded-md border-2 z-10 border-blue-500 bg-blue-500  hover:border-blue-100 hover:bg-black transition-colors" href="/borrow">
+                    <a class="mr-6 text-center text-[2rem] w-full px-10 py-4 lg:py-8 rounded-md border-2 z-10 border-blue-500 bg-blue-500 hover:border-blue-100 hover:bg-[var(--black)] transition-colors" href="/borrow">
                         Browse 
                         <br class="block lg:hidden">
                         Supplies
                     </a>
-                    <a class="mr-6 text-center text-[2rem] w-full px-10 py-4 lg:py-8 rounded-md border-2 z-10 bg-black  border-blue-100 hover:border-blue-500 transition-colors" href="/offer">
+                    <a class="mr-6 text-center text-[2rem] w-full px-10 py-4 lg:py-8 rounded-md border-2 z-10 bg-[var(--black)] border-blue-100 hover:border-blue-500 transition-colors" href="/offer">
                         Offer 
                         <br class="block lg:hidden">
                         Supplies
                     </a>
-                    <a class="mr-6 text-center text-[2rem] w-full px-10 py-4 lg:py-8 rounded-md border-2 z-10 bg-black border-blue-100 hover:border-green-500 transition-colors" href="/ask-for">
+                    <a class="mr-6 text-center text-[2rem] w-full px-10 py-4 lg:py-8 rounded-md border-2 z-10 bg-[var(--black)] border-blue-100 hover:border-green-500 transition-colors" href="/ask-for">
                         Ask 
                         <br class="block lg:hidden">
                         <span class="whitespace-nowrap">For Supplies</span>
@@ -41,23 +41,23 @@
                 <div class="w-[550px] h-[550px] rounded-full bg-blue-500 absolute bg-[url('/public/imgs/heroCircle.png')] -right-[275px] bottom-0 z-20">
 
                 </div>
-                <div class="bg-red-500 w-[600px] h-[600px] absolute bg-[url('/public/imgs/heroTriangle.png')] -bottom-[300px] -right-[0] rotate-[40deg] rounded-md">
+                <div class="bg-red-600 w-[600px] h-[600px] absolute bg-[url('/public/imgs/heroTriangle.png')] -bottom-[300px] -right-[0] rotate-[40deg] rounded-md">
 
                 </div>
             </div>
         </section>
-        <article id="about-home" class="">
-            <div class=" min-h-fit -ml-5 w-screen bg-sky-900 bg-no-repeat bg-center bg-contain bg-[url('/public/imgs/schoolsupplies.svg')]  grid grid-cols-2 grid-rows-2 mb-4 relative mt-10">
-                <div class="school-supplies-background absolute top-1/3 h-2/3 sm:top-1/2">
+        <article id="section--home-about" class="section--home-about text-[2rem]">
+            <div class="min-h-fit -ml-5 w-screen bg-sky-900 bg-no-repeat bg-center bg-contain bg-[url('/public/imgs/schoolsupplies.svg')]  grid grid-cols-2 grid-rows-2 mb-4 relative mt-10">
+                <div class="school-supplies-background absolute top-1/3 h-2/3">
                     
                 </div>
-                <div class="col-span-2 xl:col-span-1 xl:row-span-2 flex flex-col justify-center items-start px-5 pt-5">
+                <div class="flex flex-col justify-center items-start px-5 pt-5">
                     <h2 class="text-3xl mb-5 font-light w-full text-center z-10">Borrow for good</h2>
                     <p class="font-thin text-lg z-10">A simple request from a student can often seem like nothing, but for some, it may represent an underlying problem that faces families around the country. Getting supplies for the classroom can be an expensive and simply unatainable as the school year carries on. We're here to connect those that can afford to give a little to help those that need a little.</p>
                     <br>
                     <p class="font-thin text-lg z-10">In the United States there are an estimated 11.6 million children living below the poverty line. For these families, purchasing school supplies is not something that fits in the budget.</p>
                 </div>
-                <div class="col-span-2 xl:col-span-1 xl:row-span-2 flex items-center justify-center px-10 xl:px-0"><div class="border-solid border-2 border-sky-500 w-5/6 xl:w-3/4 bg-sky-900 h-72 sm:h-3/4 md:max-h-[200px] relative">
+                <div class=" flex items-center justify-center px-10"><div class="border-solid border-2 border-sky-500 w-5/6 xl:w-3/4 bg-sky-900 h-72 sm:h-3/4 md:max-h-[200px] relative">
                     <div class="border-solid border-2 border-sky-500 h-full w-full bg-sky-900 absolute right-5 top-5">
                         <div class="w-full h-full text-center py-5 px-2">
                             <h3 class="mb-3">Cost of School Supplies
@@ -93,7 +93,7 @@
             </div>
             <div class="w-full col-span-2 pb-5 px-5 row-span-1 z-10">
                 <br>
-                <p class="font-thin text-lg">According to a
+                <p class="font-thin">According to a
                     <a target="_blank" href="https://www.lendingtree.com/credit-cards/study/back-to-school-spending-leads-many-into-debt/#:~:text=1%20in%203%20parents%20of,school%20supplies%20as%20expenses%20increase.">
                     <i> 2021 study by Lending Tree</i>
                     </a>, 1 in 3 parents of school-age children expect to go into debt when shopping for back-to-school supplies. For parents with children attending schools that require hybrid learning due to the pandemic, 42% believe they'll go in debt while paying for school supplies.
@@ -102,73 +102,73 @@
         </article>
     </main>
     <script>
-    const words = [
-        { item: "pencil", color: "text-red-500" },
-        { item: "notebook", color: "text-blue-500" },
-        { item: "ruler", color: "text-green-500" },
-        { item: "highlighter", color: "text-yellow-500" },
-        { item: "backpack", color: "text-purple-500" },
-        { item: "calculator", color: "text-slate-500" },
-        { item: "binder", color: "text-pink-500" },
-    ];
+        const words = [
+            { item: "pencil", color: "text-red-500" },
+            { item: "notebook", color: "text-blue-500" },
+            { item: "ruler", color: "text-green-500" },
+            { item: "highlighter", color: "text-yellow-500" },
+            { item: "backpack", color: "text-purple-500" },
+            { item: "calculator", color: "text-slate-500" },
+            { item: "binder", color: "text-pink-500" },
+        ];
 
-    const wordContainer = document.getElementById('hero-word-rotation--container');
-    const handleTransform = (idx, interval) => {
-        var opacityGoing = 1;
-        var opacityComing = 0;
-        var rotation = 0;
+        const wordContainer = document.getElementById('hero-word-rotation--container');
+        const handleTransform = (idx, interval) => {
+            var opacityGoing = 1;
+            var opacityComing = 0;
+            var rotation = 0;
 
-        interval = setInterval(() => {
-            // setTransformInterval(idx);
-            idx++;
-            opacityGoing -= 0.01;
-            opacityComing += 0.005;
-            rotation -= 0.75;
-            rotation <= -100 ? rotation = -100 : rotation = rotation;
-            wordContainer.firstElementChild.style.transform = `translateY(${rotation}%)`
-            wordContainer.lastElementChild.style.transform = `translateY(${rotation}%)`
-            wordContainer.firstElementChild.style.opacity = opacityGoing;
-            wordContainer.lastElementChild.style.opacity = opacityComing;
-                if (idx >= 100) {
-                    idx = 100;
-                }
-            }, 1);
+            interval = setInterval(() => {
+                // setTransformInterval(idx);
+                idx++;
+                opacityGoing -= 0.01;
+                opacityComing += 0.005;
+                rotation -= 0.75;
+                rotation <= -100 ? rotation = -100 : rotation = rotation;
+                wordContainer.firstElementChild.style.transform = `translateY(${rotation}%)`
+                wordContainer.lastElementChild.style.transform = `translateY(${rotation}%)`
+                wordContainer.firstElementChild.style.opacity = opacityGoing;
+                wordContainer.lastElementChild.style.opacity = opacityComing;
+                    if (idx >= 100) {
+                        idx = 100;
+                    }
+                }, 1);
+
+                setTimeout(() => {
+                clearInterval(interval);
+                // setTransformInterval(0);
+                }, 1000);
+        };
+
+        var handleTextInterval;
+        var transformEl;
+
+        var i = 1;
+        var j = 2;
+
+        handleTextInterval = setInterval(() => {
+            if (location.pathname !== "/") {
+                clearInterval(handleTextInterval);
+            }
+            var YIdx = 0;
+            handleTransform(YIdx, transformEl);
 
             setTimeout(() => {
-            clearInterval(interval);
-            // setTransformInterval(0);
+                const newArr = [words[i], words[j]];
+                console.log(newArr)
+                wordContainer.innerHTML = "";
+                wordContainer.innerHTML = `                        
+                            <span class="${words[i].color} block" style="opacity: 1">${words[i].item}<span class="${words[i].color}">?</span></span>
+                            <span class="${words[j].color} block" style="opacity: 0">${words[j].item}<span class="${words[j].color}">?</span></span>`;
+                i++;
+                j++;
+                if (j >= words.length) {
+                    j = 0;
+                }
+                if (i >= words.length) {
+                    i = 0;
+                }
             }, 1000);
-    };
-
-    var handleTextInterval;
-    var transformEl;
-
-    var i = 1;
-    var j = 2;
-
-    handleTextInterval = setInterval(() => {
-        if (location.pathname !== "/") {
-            clearInterval(handleTextInterval);
-        }
-        var YIdx = 0;
-        handleTransform(YIdx, transformEl);
-
-        setTimeout(() => {
-            const newArr = [words[i], words[j]];
-            console.log(newArr)
-            wordContainer.innerHTML = "";
-            wordContainer.innerHTML = `                        
-                        <span class="${words[i].color} block" style="opacity: 1">${words[i].item}<span class="${words[i].color}">?</span></span>
-                        <span class="${words[j].color} block" style="opacity: 0">${words[j].item}<span class="${words[j].color}">?</span></span>`;
-            i++;
-            j++;
-            if (j >= words.length) {
-                j = 0;
-            }
-            if (i >= words.length) {
-                i = 0;
-            }
-        }, 1000);
-    }, 3500);
+        }, 3500);
     </script>
 </x-layout>
